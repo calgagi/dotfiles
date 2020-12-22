341516337ae433ef77d77914ce4e004672f3ed3a
calgagi <calgagi@gmail.com>
2020-12-21 20:24:50 -0800

    updated to work with multiple monitors

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -7,7 +7,9 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar right --config=~/.config/polybar/config &
-polybar left --config=~/.config/polybar/config &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITORR=$m polybar --reload right --config=~/.config/polybar/config & 
+    MONITORL=$m polybar --reload left --config=~/.config/polybar/config & 
+done
 
 echo "Polybar launched..."
