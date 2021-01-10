60817f89546fb1c8cb0dd40760b7da2b71195c90
calgagi <calgagi@gmail.com>
2021-01-09 16:14:36 -0800

    modified collect

--- collect.sh
+++ collect.sh
@@ -5,6 +5,7 @@ cp -r ~/.config/rofi ./config/
 cp -r ~/.config/polybar ./config/
 cp -r ~/.config/nvim ./config/
 cp -r ~/.config/picom.conf ./config/
+cp -r ~/.config/BetterDiscord/themes ./config/BetterDiscord/
 
 cp ~/.Xresources .
 cp ~/.bashrc .
